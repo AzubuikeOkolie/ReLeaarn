36a0ceec7dc2f99efdb2d4b0f12c6e21e52194f1
Kevin <Kevin@OAK>
2024-12-28 12:43:58 +0100

    Add project files.

---
+++ Chapter01/Car.cs
@@ -0,0 +1,23 @@
+﻿using static System.Math;
+namespace Chapter01
+{
+    public class Car
+    {
+        public string Colour { get; set; }
+        public string Model { get; set; }
+
+        public void Drive() 
+        {
+                Console.WriteLine($"{Model} is { Colour}");
+        }
+    }
+
+    public class Calc
+    {
+        public static double Pythagorean( double x, double y)
+        {
+            double cSqured = Pow(x, 2) + Pow(y, 2);
+            return Sqrt(cSqured);
+        }
+    }
+}
---
+++ Chapter01/Greetings.cs
@@ -0,0 +1,33 @@
+﻿using Chapter01;
+
+class Greetings
+{
+    static void Main()
+    {
+        Car car1 = new Car();
+        car1.Colour = "Red";
+        car1.Model = "Mazda";
+
+        Console.WriteLine($"Car 1: {car1.Colour} {car1.Model}");
+        car1.Drive();
+
+        Car car2 = new()
+        {
+            Colour = "Blue",
+            Model = "Toyota"
+        };
+
+        Console.WriteLine($"Car 2 Colour is {car2.Colour} and the model is {car2.Model}");
+        car2.Drive();
+
+        double a = 2.1;
+        double b = 2.1;
+
+        var calculate = Calc.Pythagorean(a, b);
+        Console.WriteLine(calculate);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+}
+
