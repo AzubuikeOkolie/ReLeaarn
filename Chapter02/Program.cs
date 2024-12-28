51710f3f02d4a9a8bdaeebfdb93884c2511886fa
Okolie Azubuike <okolieazubuike@outlook.com>
2024-12-28 13:05:45 +0100

    tenary operator

--- Chapter02/Program.cs
+++ Chapter02/Program.cs
@@ -5,6 +5,17 @@
         static void Main()
         {
             Console.WriteLine("Hello");
+            PriceChecker();
+        }
+    
+
+
+        private static void PriceChecker()
+        {
+            double price = 0;
+            string response = price == 0 ? "Sell" : "Buy";
+
+            Console.WriteLine(response);
         }
     }
 }
