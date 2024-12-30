8b8a8fe976f35b72f6dab7e1d21fcfca7a8bac71
Okolie Azubuike <okolieazubuike@outlook.com>
2024-12-30 12:00:04 +0100

    code cleanup

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,7 +2,8 @@
 {
     private static void Main(string[] args)
     {
-        Calculator calculator = new Calculator();
+        // Removed because we are now using static modifier
+        // Calculator calculator = new();
         var calResult = Calculator.AddNumber(7, 7);
         Console.WriteLine($"Result of 7 + 7 = {calResult}");
 
@@ -10,9 +11,19 @@
         var programmerCalcResult = ProgrammerCalculator.Power(4, 5);
         Console.WriteLine($"Power of 4, 5 from {programmerCalculator} and answer is {programmerCalcResult}");
 
+
+        // Inheritance usage
+        double reponse = ProgrammerCalculator.AddNumber(5, 5);
+        Console.WriteLine($"Inheritance result, asnwer = {reponse}");
+
+
+
         ScientificCalculator scientificCalculator = new();
         var scientificCalcResult = ScientificCalculator.Or(90, 5);
         Console.WriteLine($"Annswer is {scientificCalcResult}");
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
     }
 
     public class Calculator()
