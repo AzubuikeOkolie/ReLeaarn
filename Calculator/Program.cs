b598143e600f1a5589afe748f25e794c428e6da0
Okolie Azubuike <okolieazubuike@outlook.com>
2024-12-30 11:45:59 +0100

    Inheritance basics

---
+++ Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+        var calResult = Calculator.AddNumber(7, 7);
+        Console.WriteLine($"Result of 7 + 7 = {calResult}");
+
+        ProgrammerCalculator programmerCalculator = new();
+        var programmerCalcResult = ProgrammerCalculator.Power(4, 5);
+        Console.WriteLine($"Power of 4, 5 from {programmerCalculator} and answer is {programmerCalcResult}");
+
+        ScientificCalculator scientificCalculator = new();
+        var scientificCalcResult = ScientificCalculator.Or(90, 5);
+        Console.WriteLine($"Annswer is {scientificCalcResult}");
+    }
+
+    public class Calculator()
+    {
+        public static double AddNumber(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    public class ProgrammerCalculator : Calculator
+    {
+        public static double Power(double num, double pow)
+        {
+            return Math.Pow(num, pow);
+        }
+    }
+
+    public class ScientificCalculator : Calculator
+    {
+        public static int Or(int num1, int num2)
+        {
+            return num1 | num2;
+        }
+    }
+}
