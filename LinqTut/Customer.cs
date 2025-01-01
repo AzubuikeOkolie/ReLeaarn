e39f74fce9bf4943ad3a980dc7e5893fb7079593
Okolie Azubuike <okolieazubuike@outlook.com>
2025-01-01 15:54:03 +0100

    for each loop and lists

---
+++ LinqTut/Customer.cs
@@ -0,0 +1,38 @@
+﻿namespace LinqTut
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Order
+    {
+        public int CustomerId { get; set; }
+        public string Description { get; set; }
+    }
+
+    public static class Company
+    {
+        static Company()
+        {
+            Customers = new List<Customer>
+            {
+                new() { Id = 1, Name = "Kevin Okolie"},
+                new Customer  { Id = 2, Name = "Azubuike Okolie"},
+                new Customer  { Id = 3, Name = "David Okolie"}
+            };
+
+            Orders = new List<Order>
+            {
+                new Order {CustomerId = 1, Description = "Rice"},
+                new Order {CustomerId = 2, Description = "Toyota Siennaa"},
+                new Order {CustomerId = 3, Description = "Brabus"},
+            };  
+        }
+
+        public static List<Customer> Customers { get; set; }
+        public static List <Order> Orders { get; set; }
+    }
+
+}
---
+++ LinqTut/Program.cs
@@ -0,0 +1,48 @@
+﻿using LinqTut;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        IEnumerable<Customer> customers = from cust in Company.Customers select cust;
+
+        foreach (Customer customer in customers)
+        {
+            Console.WriteLine(customer.Name);
+        }
+
+        List<int> nums = new List<int>
+        {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        };
+
+        foreach (int num in nums)
+        {
+            Console.WriteLine($"Numbers in nums are {num}");
+        }
+
+        List<Order> orders = new()
+        {
+             new Order  { CustomerId = 9, Description = "No addition" },
+             new Order { CustomerId = 20, Description = "Bottle" },
+             new Order { CustomerId = 30, Description = "Sneakers" }
+        };
+
+        foreach (Order order in orders) 
+        {
+            Console.WriteLine($"Orders in list are {order.CustomerId} : {order.Description} ");
+        }
+
+        List<Customer> customers1 = new()
+        {
+            new Customer {Id = 1, Name = " Azubuike Okolie Kevin"},
+            new Customer {Id = 2, Name = " Ijeoma Okolie Pearl"},
+            new Customer {Id = 3, Name = " David Okolie Chukwunonso"}
+        };
+
+        foreach(Customer cus1 in customers1)
+        {
+            Console.WriteLine(cus1.Id + cus1.Name);
+        }
+    }
+}
